92f77b147df32d27f51324e2cfc837071e1cd35c
iamjzk <iamjzk@gmail.com>
2016-09-26 21:10:45 +0800

    zzz

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var app = require('express')(),
+    conf = require('./lib/config'),
+    wechat = require('./lib/wechat'),
     bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
@@ -6,6 +8,10 @@ app.use(bodyParser.urlencoded({
 }
 
 ));
+
+wechat(conf.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
