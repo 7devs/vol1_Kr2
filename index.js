e07a615b07f8125fa9c3367c638fe7ab261f226b
iamjzk <iamjzk@gmail.com>
2016-10-21 00:12:05 +0800

    ejs

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
+app.set('views', path.join(__dirname, 'lib/views'));
+// 设置模版引擎
+app.set('view engine', 'ejs');
+
 app.listen(8004,function(err){
   console.log('listening at 8004...')
 });
